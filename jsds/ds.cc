a60eaacf490438e55e978ed66dd9a35cc5c32d88
Hao Zhang <hzhangxyz@outlook.com>
2025-07-04 10:04:54 +0800

    Add js binding

---
+++ jsds/ds.cc
@@ -0,0 +1,138 @@
+#include <emscripten/bind.h>
+
+#include <ds/item.hh>
+#include <ds/list.hh>
+#include <ds/rule.hh>
+#include <ds/string.hh>
+#include <ds/term.hh>
+#include <ds/utility.hh>
+#include <ds/variable.hh>
+
+namespace em = emscripten;
+
+template<typename T>
+auto from_string(const std::string& text, int buffer_size) -> std::unique_ptr<T> {
+    auto result = reinterpret_cast<T*>(operator new(buffer_size));
+    auto scan_result = result->scan(text.c_str(), reinterpret_cast<std::byte*>(result) + buffer_size);
+    if (scan_result == nullptr) {
+        operator delete(result);
+        return std::unique_ptr<T>(nullptr);
+    }
+    return std::unique_ptr<T>(result);
+}
+
+template<typename T>
+auto to_string(T* value, int buffer_size) -> std::string {
+    auto result = reinterpret_cast<char*>(operator new(buffer_size));
+    auto print_result = value->print(result, reinterpret_cast<char*>(result) + buffer_size);
+    if (print_result == nullptr || print_result - result == buffer_size - 1) {
+        operator delete(result);
+        return std::string();
+    }
+    *print_result = 0;
+    auto string = std::string(result);
+    operator delete(result);
+    return string;
+}
+
+template<typename T>
+auto from_binary(const std::string& text) -> std::unique_ptr<T> {
+    auto dst = std::unique_ptr<T>(reinterpret_cast<T*>(operator new(text.size())));
+    memcpy(dst.get(), text.data(), text.size());
+    return dst;
+}
+
+template<typename T>
+auto to_binary(T* value) -> std::string {
+    return std::string(reinterpret_cast<char*>(value), value->data_size());
+}
+
+template<typename T>
+auto clone(T* value) -> std::unique_ptr<T> {
+    auto result = std::unique_ptr<T>(reinterpret_cast<T*>(operator new(value->data_size())));
+    memcpy(result.get(), value, value->data_size());
+    return result;
+}
+
+template<typename T>
+auto data_size(T* value) -> int {
+    return value->data_size();
+}
+
+template<typename T>
+auto common_declaration(em::class_<T>& t) {
+    t.class_function("from_string", from_string<T>);
+    t.class_function("to_string", to_string<T>, em::allow_raw_pointers());
+    t.class_function("from_binary", from_binary<T>);
+    t.class_function("to_binary", to_binary<T>, em::allow_raw_pointers());
+    t.function("clone", clone<T>, em::allow_raw_pointers());
+    t.function("data_size", data_size<T>, em::allow_raw_pointers());
+}
+
+auto term_ground(ds::term_t* term, ds::term_t* dictionary, int length) -> std::unique_ptr<ds::term_t> {
+    auto result = reinterpret_cast<ds::term_t*>(operator new(length));
+    if (result->ground(term, dictionary, reinterpret_cast<std::byte*>(result) + length) == nullptr) {
+        operator delete(result);
+        return std::unique_ptr<ds::term_t>(nullptr);
+    }
+    return std::unique_ptr<ds::term_t>(result);
+}
+
+auto rule_ground(ds::rule_t* rule, ds::rule_t* dictionary, int length) -> std::unique_ptr<ds::rule_t> {
+    ds::rule_t* result = reinterpret_cast<ds::rule_t*>(operator new(length));
+    if (result->ground(rule, dictionary, reinterpret_cast<std::byte*>(result) + length) == nullptr) {
+        operator delete(result);
+        return std::unique_ptr<ds::rule_t>(nullptr);
+    }
+    return std::unique_ptr<ds::rule_t>(result);
+}
+
+auto rule_match(ds::rule_t* rule_1, ds::rule_t* rule_2, int length) -> std::unique_ptr<ds::rule_t> {
+    ds::rule_t* result = reinterpret_cast<ds::rule_t*>(operator new(length));
+    if (result->match(rule_1, rule_2, reinterpret_cast<std::byte*>(result) + length) == nullptr) {
+        operator delete(result);
+        return std::unique_ptr<ds::rule_t>(nullptr);
+    }
+    return std::unique_ptr<ds::rule_t>(result);
+}
+
+EMSCRIPTEN_BINDINGS(ds) {
+    auto string_t = em::class_<ds::string_t>("String");
+    auto item_t = em::class_<ds::item_t>("Item");
+    auto variable_t = em::class_<ds::variable_t>("Variable");
+    auto list_t = em::class_<ds::list_t>("List");
+    auto term_t = em::class_<ds::term_t>("Term");
+    auto rule_t = em::class_<ds::rule_t>("Rule");
+
+    common_declaration(string_t);
+    common_declaration(item_t);
+    common_declaration(variable_t);
+    common_declaration(list_t);
+    common_declaration(term_t);
+    common_declaration(rule_t);
+
+    item_t.function("name", &ds::item_t::name, em::return_value_policy::reference());
+
+    variable_t.function("name", &ds::variable_t::name, em::return_value_policy::reference());
+
+    list_t.function("length", &ds::list_t::get_list_size);
+    list_t.function("getitem", &ds::list_t::term, em::return_value_policy::reference());
+
+    em::enum_<ds::term_type_t>("TermType")
+        .value("Variable", ds::term_type_t::variable)
+        .value("Item", ds::term_type_t::item)
+        .value("List", ds::term_type_t::list)
+        .value("Null", ds::term_type_t::null);
+    term_t.function("get_type", &ds::term_t::get_type);
+    term_t.function("variable", &ds::term_t::variable, em::return_value_policy::reference());
+    term_t.function("item", &ds::term_t::item, em::return_value_policy::reference());
+    term_t.function("list", &ds::term_t::list, em::return_value_policy::reference());
+
+    rule_t.function("length", &ds::rule_t::premises_count);
+    rule_t.function("conclusion", &ds::rule_t::conclusion, em::return_value_policy::reference());
+    rule_t.function("getitem", &ds::rule_t::premises, em::return_value_policy::reference());
+
+    term_t.class_function("ground", term_ground, em::return_value_policy::take_ownership());
+    rule_t.class_function("ground", rule_ground, em::return_value_policy::take_ownership());
+    rule_t.class_function("match", rule_match, em::return_value_policy::take_ownership());
+}
---
+++ jsds/jsds.mjs
@@ -0,0 +1,170 @@
+/* jshint esversion:9 */
+
+import createDs from './ds.mjs';
+
+const ds = await createDs(); // jshint ignore:line
+
+var _buffer_size = 1024;
+
+export function buffer_size(size = 0) {
+    const old_buffer_size = _buffer_size;
+    if (size != 0) {
+        _buffer_size = size;
+    }
+    return old_buffer_size;
+}
+
+class _common_t {
+    constructor(type, value, size = 0) {
+        this.type = type;
+        if (value instanceof _common_t) {
+            this.value = value.value;
+            this.capacity = value.capacity;
+            if (size != 0) {
+                throw new Error("Cannot set capacity when copying from another instance.");
+            }
+        } else if (value instanceof this.type) {
+            this.value = value;
+            this.capacity = size;
+        } else if (value instanceof Uint8Array) {
+            this.value = this.type.from_binary(value);
+            this.capacity = this.size();
+            if (size != 0) {
+                throw new Error("Cannot set capacity when initializing from bytes.");
+            }
+        } else if (typeof value == "string") {
+            this.capacity = size != 0 ? size : buffer_size();
+            this.value = this.type.from_string(value, this.capacity);
+            if (this.value == null) {
+                throw new Error("Initialization from a string failed.");
+            }
+        } else {
+            throw new Error("Unsupported type for initialization.");
+        }
+    }
+
+    toString() {
+        const result = this.type.to_string(this.value, buffer_size());
+        if (result == "") {
+            throw new Error("Conversion to string failed.");
+        }
+        return result;
+    }
+
+    data() {
+        const encoder = new TextEncoder();
+        return encoder.encode(this.type.to_binary(this.value));
+    }
+
+    size() {
+        return this.value.data_size();
+    }
+
+    copy() {
+        return new this.constructor(this.value.clone(), this.size());
+    }
+}
+
+export class string_t extends _common_t {
+    constructor(value, size = 0) {
+        super(ds.String, value, size);
+    }
+}
+
+export class variable_t extends _common_t {
+    constructor(value, size = 0) {
+        super(ds.Variable, value, size);
+    }
+
+    name() {
+        return new string_t(this.value.name());
+    }
+}
+
+export class item_t extends _common_t {
+    constructor(value, size = 0) {
+        super(ds.Item, value, size);
+    }
+
+    name() {
+        return new string_t(this.value.name());
+    }
+}
+
+export class list_t extends _common_t {
+    constructor(value, size = 0) {
+        super(ds.List, value, size);
+    }
+
+    length() {
+        return this.value.length();
+    }
+
+    getitem(index) {
+        return new term_t(this.value.getitem(index));
+    }
+}
+
+export class term_t extends _common_t {
+    constructor(value, size = 0) {
+        super(ds.Term, value, size);
+    }
+
+    term() {
+        const term_type = this.value.get_type();
+        if (term_type == ds.TermType.Variable) {
+            return new variable_t(this.value.variable());
+        } else if (term_type == ds.TermType.Item) {
+            return new item_t(this.value.item());
+        } else if (term_type == ds.TermType.List) {
+            return new list_t(this.value.list());
+        } else {
+            throw new TypeError("Unexpected term type.");
+        }
+    }
+
+    ground(other) {
+        const capacity = buffer_size();
+        const term = this.type.ground(this.value, other.value, capacity);
+        if (term == null) {
+            return null;
+        }
+        return new this.constructor(term, capacity);
+    }
+}
+
+export class rule_t extends _common_t {
+    constructor(value, size = 0) {
+        super(ds.Rule, value, size);
+    }
+
+    length() {
+        return this.value.length();
+    }
+
+    getitem(index) {
+        return new term_t(this.value.getitem(index));
+    }
+
+    conclusion() {
+        return new term_t(this.value.conclusion());
+    }
+
+    ground(other) {
+        const capacity = buffer_size();
+        const rule = this.type.ground(this.value, other.value, capacity);
+        if (rule == null) {
+            return null;
+        }
+        return new this.constructor(rule, capacity);
+    }
+
+    match(other) {
+        const capacity = buffer_size();
+        const rule = this.type.match(this.value, other.value, capacity);
+        if (rule == null) {
+            return null;
+        }
+        return new this.constructor(rule, capacity);
+    }
+}
