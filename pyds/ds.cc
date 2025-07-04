836bf627fb3100082d89274d0c22051b4d24874c
Hao Zhang <hzhangxyz@outlook.com>
2025-07-04 09:51:12 +0800

    Use operator new instead of malloc.

--- pyds/ds.cc
+++ pyds/ds.cc
@@ -11,33 +11,33 @@
 namespace py = pybind11;
 
 template<typename T>
-auto from_string(std::string& text, int buffer_size) -> ds::unique_malloc_ptr<T> {
-    auto result = reinterpret_cast<T*>(malloc(buffer_size));
+auto from_string(const std::string& text, int buffer_size) -> std::unique_ptr<T> {
+    auto result = reinterpret_cast<T*>(operator new(buffer_size));
     auto scan_result = result->scan(text.c_str(), reinterpret_cast<std::byte*>(result) + buffer_size);
     if (scan_result == nullptr) {
-        free(result);
-        return ds::unique_malloc_ptr<T>(nullptr);
+        operator delete(result);
+        return std::unique_ptr<T>(nullptr);
     }
-    return ds::unique_malloc_ptr<T>(result);
+    return std::unique_ptr<T>(result);
 }
 
 template<typename T>
 auto to_string(T* value, int buffer_size) -> py::str {
-    auto result = reinterpret_cast<char*>(malloc(buffer_size));
+    auto result = reinterpret_cast<char*>(operator new(buffer_size));
     auto print_result = value->print(result, reinterpret_cast<char*>(result) + buffer_size);
     if (print_result == nullptr || print_result - result == buffer_size - 1) {
-        free(result);
+        operator delete(result);
         return py::str();
     }
     *print_result = 0;
     auto string = py::str(result);
-    free(result);
+    operator delete(result);
     return string;
 }
 
 template<typename T>
-auto from_binary(std::string& text) -> ds::unique_malloc_ptr<T> {
-    auto dst = ds::unique_malloc_ptr<T>(reinterpret_cast<T*>(malloc(text.size())));
+auto from_binary(const std::string& text) -> std::unique_ptr<T> {
+    auto dst = std::unique_ptr<T>(reinterpret_cast<T*>(operator new(text.size())));
     memcpy(dst.get(), text.data(), text.size());
     return dst;
 }
@@ -48,10 +48,15 @@ auto to_binary(T* value) -> py::bytes {
 }
 
 template<typename T>
-auto clone(T* value) -> ds::unique_malloc_ptr<T> {
-    auto result = ds::unique_malloc_ptr<T>(reinterpret_cast<T*>(malloc(value->data_size())));
+auto clone(T* value) -> std::unique_ptr<T> {
+    auto result = std::unique_ptr<T>(reinterpret_cast<T*>(operator new(value->data_size())));
     memcpy(result.get(), value, value->data_size());
     return result;
+}
+
+template<typename T>
+auto data_size(T* value) -> int {
+    return value->data_size();
 }
 
 template<typename T>
@@ -61,7 +66,34 @@ auto common_declaration(py::class_<T>& t) {
     t.def_static("from_binary", from_binary<T>);
     t.def_static("to_binary", to_binary<T>);
     t.def("clone", clone<T>);
-    t.def("data_size", [](T* v) { return v->data_size(); });
+    t.def("data_size", data_size<T>);
+}
+
+auto term_ground(ds::term_t* term, ds::term_t* dictionary, int length) -> std::unique_ptr<ds::term_t> {
+    auto result = reinterpret_cast<ds::term_t*>(operator new(length));
+    if (result->ground(term, dictionary, reinterpret_cast<std::byte*>(result) + length) == nullptr) {
+        operator delete(result);
+        return std::unique_ptr<ds::term_t>(nullptr);
+    }
+    return std::unique_ptr<ds::term_t>(result);
+}
+
+auto rule_ground(ds::rule_t* rule, ds::rule_t* dictionary, int length) -> std::unique_ptr<ds::rule_t> {
+    ds::rule_t* result = reinterpret_cast<ds::rule_t*>(operator new(length));
+    if (result->ground(rule, dictionary, reinterpret_cast<std::byte*>(result) + length) == nullptr) {
+        operator delete(result);
+        return std::unique_ptr<ds::rule_t>(nullptr);
+    }
+    return std::unique_ptr<ds::rule_t>(result);
+}
+
+auto rule_match(ds::rule_t* rule_1, ds::rule_t* rule_2, int length) -> std::unique_ptr<ds::rule_t> {
+    ds::rule_t* result = reinterpret_cast<ds::rule_t*>(operator new(length));
+    if (result->match(rule_1, rule_2, reinterpret_cast<std::byte*>(result) + length) == nullptr) {
+        operator delete(result);
+        return std::unique_ptr<ds::rule_t>(nullptr);
+    }
+    return std::unique_ptr<ds::rule_t>(result);
 }
 
 PYBIND11_MODULE(ds, m) {
@@ -126,7 +158,7 @@ PYBIND11_MODULE(ds, m) {
         py::return_value_policy::reference_internal
     );
 
-    term_t.def_static("ground", [](ds::term_t* term, ds::term_t* dictionary, int buffer_size) { return ds::ground(term, dictionary, buffer_size); });
-    rule_t.def_static("ground", [](ds::rule_t* rule, ds::rule_t* dictionary, int buffer_size) { return ds::ground(rule, dictionary, buffer_size); });
-    rule_t.def_static("match", [](ds::rule_t* rule_1, ds::rule_t* rule_2, int buffer_size) { return ds::match(rule_1, rule_2, buffer_size); });
+    term_t.def_static("ground", term_ground);
+    rule_t.def_static("ground", rule_ground);
+    rule_t.def_static("match", rule_match);
 }
