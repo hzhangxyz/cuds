9259864f7bb809de6280a8bdeccc113fa8befb89
Hao Zhang <hzhangxyz@outlook.com>
2025-07-05 18:36:23 +0800

    Add tests for config.hh, helper.hh, string.hh and string.cc.

--- src/string.cc
+++ src/string.cc
@@ -1,5 +1,6 @@
 #include <cstring>
 
+#include <ds/helper.hh>
 #include <ds/string.hh>
 
 namespace ds {
@@ -16,22 +17,10 @@ namespace ds {
     }
 
     string_t* string_t::set_length(length_t length, std::byte* check_tail) {
-        // 检查对象能否存下长度数据
-        // 考虑极限情况，长度为0（虽然字符串长度不可能为0,有\0的尾巴）时，check_tail == get_string()
-        // 所以这里我们只需要保证 check_tail >= get_string() 即可
-        if (check_tail != nullptr) {
-            if (check_tail < reinterpret_cast<std::byte*>(get_string())) {
-                return nullptr;
-            }
+        if (check_before_fail(check_tail, string_pointer(), length)) [[unlikely]] {
+            return nullptr;
         }
         *length_pointer() = length;
-        // 检查对象能否存下字符串数据
-        // tail是最后一字节数据后面的那个位置，所以check_tail == tail()是可以的
-        if (check_tail != nullptr) {
-            if (check_tail < tail()) {
-                return nullptr;
-            }
-        }
         return this;
     }
 
@@ -48,7 +37,7 @@ namespace ds {
                 dst[index] = 0;
             } else {
                 dst[index] = src[index];
-                if (src[index] == 0) {
+                if (src[index] == 0) [[unlikely]] {
                     end = true;
                 }
             }
@@ -58,7 +47,11 @@ namespace ds {
     }
 
     string_t* string_t::set_null_string(const char* buffer, std::byte* check_tail) {
-        set_length(strlen(buffer) + 1, check_tail);
+        length_t length = strlen(buffer) + 1;
+        if (check_before_fail(check_tail, string_pointer(), length)) [[unlikely]] {
+            return nullptr;
+        }
+        set_length(length, nullptr);
         set_string(buffer);
         return this;
     }
@@ -77,16 +70,14 @@ namespace ds {
 
     char* string_t::print(char* buffer, char* check_tail) {
         char* src = get_string();
+        char* dst = buffer;
         while (*src) {
-            // 写前检查，如果buffer已经超过了check_tail(等于也会失败)，则失败
-            if (check_tail != nullptr) {
-                if (check_tail <= buffer) {
-                    return nullptr;
-                }
+            if (check_till_fail(check_tail, dst)) [[unlikely]] {
+                return nullptr;
             }
-            *(buffer++) = *(src++);
+            *(dst++) = *(src++);
         }
-        return buffer;
+        return dst;
     }
 
     const char* string_t::scan(const char* buffer, std::byte* check_tail) {
@@ -96,22 +87,15 @@ namespace ds {
             if (strchr("'() \t\r\n", *src) != nullptr) {
                 break;
             }
-            // 写前检查，如果dst已经超过了check_tail(等于也会失败)，则失败
-            if (check_tail != nullptr) {
-                if (check_tail <= reinterpret_cast<std::byte*>(dst)) {
-                    return nullptr;
-                }
+            if (check_till_fail(check_tail, dst)) [[unlikely]] {
+                return nullptr;
             }
             *(dst++) = *(src++);
         }
-        // 最后一个字符是'\0'，也需要检查是否超过了check_tail
-        if (check_tail != nullptr) {
-            if (check_tail <= reinterpret_cast<std::byte*>(dst)) {
-                return nullptr;
-            }
+        if (check_till_fail(check_tail, dst)) [[unlikely]] {
+            return nullptr;
         }
         *dst = 0;
-        // 已经在后面放置数据了，不需要检查前面的空间是否足够
         set_length(strlen(get_string()) + 1, nullptr);
         return src;
     }
