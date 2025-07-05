6d6b88fa7aff6efcc732cafbae0b3050c85a8fdc
Hao Zhang <hzhangxyz@outlook.com>
2025-07-05 13:17:23 +0800

    Prepare cmake for gtest and use cmake to build python binding.

--- pyds/version.py
+++ pyds/version.py
@@ -5,5 +5,9 @@ __all__ = [
 
 try:
     from ._version import version, __version__
-except ImportError:
-    version = __version__ = "0.0.0"
+except ModuleNotFoundError:
+    try:
+        import importlib.metadata
+        version = __version__ = importlib.metadata.version("pyds")
+    except importlib.metadata.PackageNotFoundError:
+        version = __version__ = "0.0.0"
--- setup.py
+++
@@ -1,8 +0,0 @@
-from glob import glob
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension
-
-sources = glob("src/*.cc") + glob("pyds/*.cc")
-ext_modules = [Pybind11Extension("pyds.ds", sources=sources, include_dirs=["include"])]
-
-setup(ext_modules=ext_modules)
